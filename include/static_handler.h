c29c76a3fdc85a37a65b84de53264ccbd0ec8b64
Devel User <eyoon1131@g.ucla.edu>
2025-04-24 11:04:43 +0000

    Set up basic static handler that serves html files in /base_directory/static. Returns with status code 404 if file doesn't exist.
    Change-Id: I57a784899ac3a0157add007bb0b6712bc5e3e488

--- include/static_handler.h
+++ include/static_handler.h
@@ -6,6 +6,8 @@
 class StaticHandler : public RequestHandler {
   public:
     Response handle_request(const Request& request) override;
+  private:
+    static const std::string base_dir_;
 };
     
 #endif // STATIC_HANDLER_H
--- src/static_handler.cc
+++ src/static_handler.cc
@@ -1,6 +1,19 @@
 #include "static_handler.h"
+#include <fstream>
+#include <sstream>
+
+const std::string StaticHandler::base_dir_ = "../base_directory";
 
 Response StaticHandler::handle_request(const Request& request) {
-  std::string body = "Bad Request";
-  return Response(request.get_version(), 400, "text/plain", body.length(), "close", body);
+  std::string file_path = base_dir_ + request.get_url();
+  std::ifstream file(file_path);
+  if (!file) {
+    std::string body = "File not found";
+    return Response(request.get_version(), 404, "text/plain", body.length(), "close", body);
+  }
+
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  std::string body = buffer.str();
+  return Response(request.get_version(), 200, "text/html", body.length(), "close", body);
 }
